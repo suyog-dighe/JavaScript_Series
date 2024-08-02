dc8abe090d956ab6ea336843f498e352d695c441
suyog-dighe <suyogbdighe@gmail.com>
2024-08-02 07:39:37 +0530

    hoisting, Error type

--- namaste_javascript.js
+++ namaste_javascript.js
@@ -6,7 +6,7 @@ function a() {
   console.log(c); // 10 it is a hoisting before initialization we can call it or print it
   //  b();   //  RangeError: Maximum call stack size exceeded
 }
-var d = 7;
+// var d = 7;
 function b() {
   var d = 7;
   // let c = 10;   // ReferenceError: c is not defined
@@ -14,4 +14,10 @@ function b() {
 }
 let c = 10;
 b();
-// console.log(d);   ReferenceError: d is not defined  here js is not allow to print d 
+// console.log(d);   ReferenceError: d is not defined  here js is not allow to print d
+
+console.log(v);  // undefined
+// console.log(s);   //ReferenceError: Cannot access 's' before initialization
+// console.log(d);     // ReferenceError: d is not defined
+let s = 966;
+var v = 669;
